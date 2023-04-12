97e9b37e503e716cc4a1b78b679bb583331f214a
Sireat <sireatag@gmail.com>
2023-04-12 16:05:43 +0300

    my commit

---
+++ hello.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+
+
+console.log('Hello java script world')
---
+++ variable.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+var name='Yaregal';
+console.log('name befor function call ', name);
+
+function changeName(){
+name ='Sireat'
+}
+changeName();
+console.log('name befor function call ', name);
+
